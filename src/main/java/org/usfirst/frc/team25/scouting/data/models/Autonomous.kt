bc39a1b24ce59b6300eb017eae93cdfb660269de
hybras <24651269+hybras@users.noreply.github.com>
2020-02-05 20:05:09 -0500

    Remove default values from Autonomous.kt

--- src/main/java/org/usfirst/frc/team25/scouting/data/models/Autonomous.kt
+++ src/main/java/org/usfirst/frc/team25/scouting/data/models/Autonomous.kt
@@ -1,12 +1,12 @@
 package org.usfirst.frc.team25.scouting.data.models
 
 data class Autonomous(
-	val cellsScoredBottom: Int = 0,
-	val cellsScoredInner: Int = 0,
-	val cellsScoredOuter: Int = 0,
-	val cellPickupRpoint: Int = 0,
-	val cellPickupTrench: Int = 0,
-	val cellsDropped: Int = 0,
-	val crossInitLine: Boolean = false,
-	val crossOpponentSector: Boolean = false
+	val cellsScoredBottom: Int,
+	val cellsScoredInner: Int,
+	val cellsScoredOuter: Int,
+	val cellPickupRpoint: Int,
+	val cellPickupTrench: Int,
+	val cellsDropped: Int,
+	val crossInitLine: Boolean,
+	val crossOpponentSector: Boolean
 )
