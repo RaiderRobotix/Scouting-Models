3453975a47f49de5c35d382955d26bdc2e9111ef
hybras <24651269+hybras@users.noreply.github.com>
2020-02-05 20:05:24 -0500

    Remove extranous comments

--- src/main/java/org/usfirst/frc/team25/scouting/data/models/TeleOp.kt
+++ src/main/java/org/usfirst/frc/team25/scouting/data/models/TeleOp.kt
@@ -5,16 +5,16 @@ package org.usfirst.frc.team25.scouting.data.models
  * Includes endgame data
  */
 data class TeleOp(
-	val cellsScoredBottom: Int,//
-	val cellsScoredInner: Int,//
-	val cellsScoredOuter: Int,//
-	val cellsDropped: Int,//
-	val rotationControl: Boolean,//
-	val rotationOverspun: Boolean,//
-	val positionControl: Boolean,//
-	val attemptHang: Boolean,//
-	val successHang: Boolean,//
-	val rungLevel: Boolean,//
+	val cellsScoredBottom: Int,
+	val cellsScoredInner: Int,
+	val cellsScoredOuter: Int,
+	val cellsDropped: Int,
+	val rotationControl: Boolean,
+	val rotationOverspun: Boolean,
+	val positionControl: Boolean,
+	val attemptHang: Boolean,
+	val successHang: Boolean,
+	val rungLevel: Boolean,
 	val assistingClimbTeamNum: Int,
 	val assistedClimbs: Int
 )
