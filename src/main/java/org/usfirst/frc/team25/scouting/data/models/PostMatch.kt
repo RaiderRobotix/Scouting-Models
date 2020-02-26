97a5a1d8d0b94c144b98bb088771091df95ecb00
TekRekon <45916994+TekRekon@users.noreply.github.com>
2020-02-26 16:08:37 -0500

    Move robotQuickComments to primary constructor by Varun

--- src/main/java/org/usfirst/frc/team25/scouting/data/models/PostMatch.kt
+++ src/main/java/org/usfirst/frc/team25/scouting/data/models/PostMatch.kt
@@ -1,7 +1,5 @@
 package org.usfirst.frc.team25.scouting.data.models
 
-import java.util.*
-
 /**
  * Qualitative reflection on the robot's performance after a match
  * Not to be used for end game actions
@@ -10,7 +8,7 @@ data class PostMatch(
 	val comparison: Comparison,
 	val pickNumber: Int = 0,
 	val robotComment: String,
-	val teleopFocus: String
-) {
-	val robotQuickCommentSelections = HashMap<String, Boolean>()
-}
+	val teleopFocus: String,
+	val robotQuickCommentSelections: Map<String, Boolean> = mapOf()
+)
+
